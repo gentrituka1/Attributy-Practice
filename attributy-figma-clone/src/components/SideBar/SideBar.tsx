600ff1bae789bb73d7ec0ca6167e8c50a14b027c
Gentrit <gentrit-uka@live.com>
2023-02-03 16:23:05 +0100

    updated

--- attributy-figma-clone/src/components/SideBar/SideBar.tsx
+++ attributy-figma-clone/src/components/SideBar/SideBar.tsx
@@ -7,22 +7,42 @@ function SideBar() {
       <button className="side-bar__button__div">
         <span className="side-bar__plus">+</span>
         <span className="side-bar__newfolder__span">New Folder</span>
-        </button>
-      <div>
-        
-        <img />
-        <p></p>
-        <button></button>
+      </button>
+      <div className="side-bar__item-div">
+        <div className="side-bar__item-div-selector"></div>
+        <div className="side-bar__item-div-child">
+          <div className="side-bar__item-image">
+            <img src="src\assets\folder.svg" />
+          </div>
+          <div className="side-bar__item-text">
+            <p>Employer</p>
+          </div>
+        </div>
+        <button>10</button>
       </div>
-      <div>
-        <img />
-        <p></p>
-        <button></button>
+      <div className="side-bar__item-div">
+        <div className="side-bar__item-div-inactive"></div>
+        <div className="side-bar__item-div-child">
+          <div className="side-bar__item-image">
+            <img src="src\assets\folder.svg" />
+          </div>
+          <div className="side-bar__item-text">
+            <p>Drafts</p>
+          </div>
+        </div>
+        <button>15</button>
       </div>
-      <div>
-        <img />
-        <p></p>
-        <button></button>
+      <div className="side-bar__item-div">
+        <div className="side-bar__item-div-inactive"></div>
+        <div className="side-bar__item-div-child">
+          <div className="side-bar__item-image">
+            <img src="src\assets\folder.svg" />
+          </div>
+          <div className="side-bar__item-text">
+            <p>Emails</p>
+          </div>
+        </div>
+        <button>12</button>
       </div>
     </aside>
   );
